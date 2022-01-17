4e394a188f812246fb48b500e3aff087b9404c53
Yevhenbk <balagutrak.jenia@gmail.com>
2022-01-17 10:09:42 +0000

    initializing

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,31 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+
+            {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "Lucky Numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "Lucky Numbers": [1]
+            }
+
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,15 +45,21 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        self._members.append(member)
+        return self
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for position, member in enumerate(self._members): 
+            if member["id"] == int(id): 
+                member_deleted = self._members.pop(position) 
+                return member_deleted
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == int(id): 
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
